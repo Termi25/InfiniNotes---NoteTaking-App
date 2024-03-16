e400806ff6a00af3db6442e45aa844437146770e
Termi25 <mariusrusu208@gmail.com>
2024-03-16 17:24:09 +0200

    Updatat elemente de UI care erau wrapContent si multiline in loc de constraintview si singleline in activitatea de adaugare generala.

--- app/src/main/java/com/ase/aplicatienotite/main/ActivitateVizualNotiteSectiune.java
+++ app/src/main/java/com/ase/aplicatienotite/main/ActivitateVizualNotiteSectiune.java
@@ -1,8 +1,6 @@
 package com.ase.aplicatienotite.main;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ase.aplicatienotite.R;
 import com.ase.aplicatienotite.adaptoare.AdapterNotita;
-import com.ase.aplicatienotite.adaptoare.AdapterSectiune;
-import com.ase.aplicatienotite.baze_date.local.database.NotiteDB;
-import com.ase.aplicatienotite.baze_date.local.view.holder.SectiuneNotiteJoinViewHolder;
 import com.ase.aplicatienotite.baze_date.local.view.model.SectiuneNotiteJoinViewModel;
-import com.ase.aplicatienotite.baze_date.local.view.model.SectiuniViewModel;
-import com.ase.aplicatienotite.clase.notite.Notita;
-import com.ase.aplicatienotite.clase.sectiune.Sectiune;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ActivitateVizualNotiteSectiune extends AppCompatActivity {
     private SectiuneNotiteJoinViewModel sectiuneNotiteJoinViewModel;
