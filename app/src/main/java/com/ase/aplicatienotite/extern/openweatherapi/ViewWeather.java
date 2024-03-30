b4a6220d1dd47ed3d1f523d9d6d378113d08ef0d
Termi25 <mariusrusu208@gmail.com>
2024-03-30 18:33:28 +0200

    Updatat View Weather pentru a prelua locatia exacta a device-ului pentru afisarea vremii + permisiunile aferente in AndroidManifest.xml

--- app/src/main/java/com/ase/aplicatienotite/extern/openweatherapi/ViewWeather.java
+++ app/src/main/java/com/ase/aplicatienotite/extern/openweatherapi/ViewWeather.java
@@ -1,7 +1,15 @@
 package com.ase.aplicatienotite.extern.openweatherapi;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +20,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,57 +36,103 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Locale;
 
 import es.dmoral.toasty.Toasty;
 
 
 public class ViewWeather extends ConstraintLayout {
     private String fileName;
+    double longitude;
+    double latitude;
+
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+    };
 
     public ViewWeather(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        View.inflate(context,R.layout.view_vreme,this);
-        try{
+        View.inflate(context, R.layout.view_vreme, this);
+        try {
             TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.ViewWeather);
             fileName = ta.getString(R.styleable.ViewWeather_setDataProvider);
-            RequestQueue req= Volley.newRequestQueue(context);
+            RequestQueue req = Volley.newRequestQueue(context);
             String url = null;
-            try{
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(context.getAssets().open("chei.txt")));
-                url=reader.readLine();
-            } catch (IOException e) {
-                Log.e("Error",getContext().getString(R.string.error_view_weather_key_file_no_read));
-            }
-            JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,
-                    url,
-                    null,
-                    (Response.Listener<JSONObject>) response->{
-                        try{
-                            JSONObject current=response.getJSONObject("current");
-                            JSONArray weather=current.getJSONArray("weather");
-                            String temp=current.getString("temp");
-                            String icon=weather.getJSONObject(0).getString("icon");
-                            incarcaImagine("https://openweathermap.org/img/wn/"+icon+"@2x.png",context,icon);
-                            TextView oras=findViewById(R.id.tvOrasTemperatura);
-                            TextView temperatura=findViewById(R.id.tvTemperatura);
-                            oras.setText("București");
-                            temperatura.setText(String.valueOf((int)Double.parseDouble(temp)));
 
-                        }catch (Exception e){
-                            Log.e("Error",getContext().getString(R.string.error_view_weather_json_read_fail));
-                        }
-                    },
-                    (Response.ErrorListener) error -> {
-                        Toasty.error(context, "Eroare! Nu se poate accesa prognoza meteo.", Toast.LENGTH_LONG).show();
-                        Log.e("Error", getContext().getString(R.string.error_view_weather_no_access));
-                    } );
-            req.add(jsonObjectRequest);
-            ta.recycle();
-        }catch (Exception e){
-            Log.e("Error",getContext().getString(R.string.error_view_weather_general_fail));
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            while (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            TextView oras = findViewById(R.id.tvOrasTemperatura);
+            if (location != null) {
+                this.longitude = location.getLongitude();
+                this.latitude = location.getLatitude();
+                url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + this.latitude + "&lon=" +
+                        this.longitude + "&exclude=hourly,daily,minutely,alerts&units=metric&appid=";
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(context.getAssets().open("cheie.txt")));
+                    url = url + reader.readLine();
+                } catch (IOException e) {
+                    Log.e("Error", getContext().getString(R.string.error_view_weather_key_file_no_read));
+                }
+
+                Geocoder geocoder=new Geocoder(context, Locale.ENGLISH);
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                    Address obj = addresses.get(0);
+                    String localitate = obj.getLocality();
+                    oras.setText(localitate);
+
+                } catch (IOException e) {
+                    Log.e("Error","eroare obtinere localitate pentru coordonatele furnizat in ViewWeather");
+                }
+            } else {
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(context.getAssets().open("default.txt")));
+                    url = reader.readLine();
+                } catch (IOException e) {
+                    Log.e("Error", getContext().getString(R.string.error_view_weather_key_file_no_read));
+                }
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                        url,
+                        null,
+                        (Response.Listener<JSONObject>) response -> {
+                            try {
+                                JSONObject current = response.getJSONObject("current");
+                                JSONArray weather = current.getJSONArray("weather");
+                                String temp = current.getString("temp");
+                                String icon = weather.getJSONObject(0).getString("icon");
+                                incarcaImagine("https://openweathermap.org/img/wn/" + icon + "@2x.png", context, icon);
+                                TextView temperatura = findViewById(R.id.tvTemperatura);
+                                oras.setText("București");
+                                temperatura.setText(String.valueOf((int) Double.parseDouble(temp)));
+                            } catch (Exception e) {
+                                Log.e("Error", getContext().getString(R.string.error_view_weather_json_read_fail));
+                            }
+                        },
+                        (Response.ErrorListener) error -> {
+                            Toasty.error(context, "Eroare! Nu se poate accesa prognoza meteo.", Toast.LENGTH_LONG).show();
+                            Log.e("Error", getContext().getString(R.string.error_view_weather_no_access));
+                        });
+                req.add(jsonObjectRequest);
+                ta.recycle();
+        }catch(Exception e){
+            Log.e("Error", getContext().getString(R.string.error_view_weather_general_fail));
         }
-        Button btnRefresh=findViewById(R.id.btnReincarcareVreme);
+        Button btnRefresh = findViewById(R.id.btnReincarcareVreme);
         btnRefresh.setOnClickListener(v -> postInvalidate());
     }
 
--- app/src/main/java/com/ase/aplicatienotite/main/activitati/ActivitateSetari.java
+++ app/src/main/java/com/ase/aplicatienotite/main/activitati/ActivitateSetari.java
@@ -1,10 +1,15 @@
 package com.ase.aplicatienotite.main.activitati;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.ase.aplicatienotite.R;
 import com.ase.aplicatienotite.main.fragmente.FragmentSetari;
@@ -19,6 +24,11 @@ public class ActivitateSetari extends AppCompatActivity {
                 .replace(R.id.fragmentContainerView, new FragmentSetari())
                 .commit();
 
+        ImageButton imgBtnShareDB=findViewById(R.id.imgBtnShareDB);
+        imgBtnShareDB.setOnClickListener(v->{
+
+        });
+
         ImageButton imgBtnSalavareSetari=findViewById(R.id.imgBtnSalvareSetari);
         imgBtnSalavareSetari.setOnClickListener(v->{
             setResult(RESULT_OK);
@@ -26,5 +36,7 @@ public class ActivitateSetari extends AppCompatActivity {
         });
     }
 
+    private static void exportDatabase(Context context,String filename){
 
+    }
 }
